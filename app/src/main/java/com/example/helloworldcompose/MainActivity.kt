676efcc4119d22c451b4aa106b99a3c9a89dc1ac
Ro <youremail@example.com>
2026-09-05 20:39:31 -0500

    Nuevo formulario JC

--- app/src/main/java/com/example/helloworldcompose/MainActivity.kt
+++ app/src/main/java/com/example/helloworldcompose/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.InputTransformation.Companion.keyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.helloworldcompose.ui.theme.HelloWorldComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +32,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HelloWorldComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Mano",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            HelloComposeForm()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HelloComposeForm(){
+    var name by remember { mutableStateOf("") }
+    var birthDate by remember { mutableStateOf("") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HelloWorldComposeTheme {
-        Greeting("Android")
+    Scaffold(
+        topBar = {
+            TopAppBar({Text("Bienvenido a ESAN")})
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ){
+            Text("Bienvenido a JetPack Compose")
+            OutlinedTextField(
+                value=name
+                , onValueChange = {name = it}
+                , label={Text("Nombre")}
+                //, keyboardOptions= KeyboardOptions(keyboardType= KeyboardType.Text)
+            )
+            OutlinedTextField(
+                value=birthDate
+                , onValueChange = {birthDate = it}
+                , label={Text("Fecha de nacimiento")}
+            )
+            Button(
+                onClick={},
+                enabled=name.isNotEmpty() && birthDate.isNotEmpty()
+            )
+            {
+                Text("Enviar")
+            }
+        }
     }
 }
